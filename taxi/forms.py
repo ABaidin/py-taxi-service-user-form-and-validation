a6b0a1e83699fcd1986396c0d6085a364878c583
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-20 18:23:07 +0300

    refactor validation form

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,17 +6,18 @@ from django.contrib.auth.forms import UserCreationForm
 class LicenseValidationMixin:
     def clean_license_number(self) -> str:
         license_number = self.cleaned_data.get("license_number")
-        if not (
+        if (
                 license_number[:3].isalpha()
                 and license_number[:3].isupper()
                 and license_number[3:].isdigit()
                 and len(license_number) == 8
         ):
+            return license_number
+        else:
             raise forms.ValidationError(
                 "License number must consist of "
                 "3 uppercase letters followed by 5 digits."
             )
-        return license_number
 
 
 class DriverCreationForm(LicenseValidationMixin, UserCreationForm):
